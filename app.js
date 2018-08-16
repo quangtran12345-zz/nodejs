0ef943d9d592acb5a490f3b0f9b895ddad330b3a
tyntin091 <tyntin091@gmail.com>
2018-08-16 15:29:21 +0700

    install packages

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var config = require('./config');
+const engine = require('ejs-locals');
+var mongoose = require('mongoose');
+require('./api/models/cinemaModel');
 var indexRouter = require('./routes/index');
+var cinemaRouter = require('./api/routes/cinema');
 var usersRouter = require('./routes/users');
-
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.engine('ejs',engine);
+mongoose.connect(config.getDbConnectionString(),{ useNewUrlParser: true });
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/cinema',cinemaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
