c9a4284b0be9b7d96a6fd6925dadcd0bba6d9326
tyntin091 <tyntin091@gmail.com>
2018-08-22 19:39:44 +0700

    fix bug

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router()
 /* GET home page. */
 
 router.get('/', function (req, res, next) {
-  res.render('cinema/home', { title: 'My Cinema' })
+  res.render('cinema/home', { title: 'My Cinema', token: ''})
 })
 
 router.get('/create', function (req, res, next) {
