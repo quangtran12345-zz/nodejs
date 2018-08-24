666ad5d445752d86de31eda71002f9a7b2a4d115
tyntin091 <tyntin091@gmail.com>
2018-08-24 13:13:30 +0700

    fix edit profile api

--- api/routes/user.js
+++ api/routes/user.js
@@ -44,10 +44,10 @@ router.post('/avatar', fileUpload(), async (req, res) => {
 router.put('/:id', async (req, res) => {
   try {
     let savedData = await userController.updateUser(req.params.id, req.body)
-    res.send({user: savedData})
+    res.send({user: req.body})
   } catch (error) {
     res.send({
-      error: error
+      error: req.body
     })
   }
 })
